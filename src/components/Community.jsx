00ba52c356c0b41329ea13a917edefe9c4e84dbb
yousef <yousefhossein50@gmail.com>
2025-04-13 19:01:57 +0330

    card

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -1,25 +1,28 @@
-import React from "react"
+import React from "react";
 import icon1 from "../assets/community/Icon1.svg";
 import icon2 from "../assets/community/Icon2.svg";
 import icon3 from "../assets/community/Icon3.svg";
 
-const Card = ({ icon , title , discription})=> {
+const Card = ({ icon, title, description }) => (
+
+  <div className=" py-5 px-6 rounded-lg w-[300px]">
     <div>
-        <div>
-            <div>
+      <div className=" relative text-center">
+        <img className=" inline-block" src={icon} alt={title} />
 
-                <img src={icon} alt={title} />
+        <span className="-z-10 absolute top-0 right-25 w-[50px] h-[49px] bg-[#cae9cc] "></span>
+      </div>
 
-                <span className=""></span>
-            </div>
-
-            <h2 className=" text-4xl text-[#4D4D4D] font-bold text-center ">{title} </h2>
-        </div>
-
-        <p className="text-[#717171] text-sm font-normal text-center">{discription}</p>
-
+      <h2 className=" text-[2rem] text-[#4D4D4D] font-bold text-center leading-9 ">
+        {title}
+      </h2>
     </div>
-}
+
+    <p className="text-[#717171] text-sm font-normal text-center my-2">
+      {description}
+    </p>
+  </div>
+);
 
 export const Community = ()=>{
     const cardData = [
@@ -41,22 +44,21 @@ export const Community = ()=>{
       ];
 
     return (
-      <>
       <div className=" text-center">
-
         <div>
-            <h1 className=" text-[#4D4D4D] text-4xl">Manage your entire community</h1>
-            <h1 className=" text-[#4D4D4D] text-4xl">in a single system</h1>
-            <p className=" font-normal text-2xl">Who is Nextcent suitable for?</p>
+          <h1 className=" text-[#4D4D4D] text-4xl font-bold mt-3">
+            Manage your entire community
+          </h1>
+          <h1 className=" text-[#4D4D4D] text-4xl font-bold">in a single system</h1>
+          <p className=" text-[#b0aeae] font-normal text-base my-3">
+            Who is Nextcent suitable for?
+          </p>
         </div>
         <div className=" flex justify-between ">
-            {cardData.map((card,index )=>{
-                <Card key={index} {...card}/>
-            })}
-
+          {cardData.map((card, index) => (
+            <Card key={index} {...card} />
+          ))}
         </div>
       </div>
-       
-      </>
     );
 }
